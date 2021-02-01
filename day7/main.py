ad92fe5e87b10589346b39c3da3c5c63aee6d0d6
Przemyslaw Lalak <przemyslaw.lalak@xcaliber.com>
2021-02-01 18:57:45 +0100

    program do zrefaktorowania

---
+++ day7/main.py
@@ -0,0 +1,9 @@
+names_list = []
+welcome_message = "Baza danych\
+\nWybierz opcję:\
+\nWyszukaj imie, naciśnij klawisz 1:\
+\nDodaj imie, naciśnij klawisz 2:\
+\nUsuń imie, naciśnij klawisz 3:\
+\nWyświetl liczbę imion, naciśnij klawisz 4:\
+\nWyświetl listę imion, naciśnij klawisz 5:\
+\nZakończ program, naciśnij klawisz 0:\n"
