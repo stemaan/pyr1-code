2e68307e920a7df3cf103ba11a3c7df22380708e
Przemyslaw Lalak <przemyslaw.lalak@xcaliber.com>
2021-02-08 19:41:34 +0100

    metoda enable_program

--- day9/class_example.py
+++ day9/class_example.py
@@ -1,8 +1,29 @@
+class WashingMachineProgram:
+    def __init__(self, name, temperature, speed, time_to_complete):
+        self.name = name
+        self.temperature = temperature
+        self.spinning_speed = speed
+        self.time = time_to_complete
+
+
 class WashingMachine:
     def __init__(self, manufacturer, model):
         self.producer = manufacturer
         self.model = model
         self.is_on = False
+        self.current_program = None
+        self.current_state = None
+        self.states = (
+            'washing',
+            'spinning',
+            'pumping_out'
+        )
+        self.programs = (
+            'daily',
+            'wool',
+            'silk',
+            'sport'
+        )
 
     def turn_on(self):
         if not self.is_on:  # 0/False -> not 0/False -> 1/True
@@ -12,18 +33,38 @@ class WashingMachine:
         if self.is_on:
             self.is_on = False
 
+    def enable_program(self, program_name):
+        if self.is_on:
+            if self.current_state is None:
+                self.current_program = program_name
+                # pralka rozpoczyna proces prania
+                self.current_state = self.states[0]
+            else:
+                print('Nie mozesz zmienic programu gdy inny juz trwa!')
+        else:
+            print('Wlacz pralke!')
 
-whirlpool_washing_machine = WashingMachine('Whirlpool', 'XXX123')
-updated_whirlpool_washing_machine = WashingMachine('Whirlpool', 'XXX124')
-beko_washing_machine = WashingMachine('Beko', '123ABC')
+    def display_current_status(self):
+        print(f'Aktualny program to: {self.current_program}, cykl: {self.current_state}')
 
-whirlpool_washing_machine.turn_on()
-print(whirlpool_washing_machine.producer, whirlpool_washing_machine.model, whirlpool_washing_machine.is_on)
 
-print(updated_whirlpool_washing_machine.producer, updated_whirlpool_washing_machine.model,
-      updated_whirlpool_washing_machine.is_on)
+if __name__ == '__main__':
+    whirlpool_washing_machine = WashingMachine('Whirlpool', 'XXX123')
+    updated_whirlpool_washing_machine = WashingMachine('Whirlpool', 'XXX124')
+    beko_washing_machine = WashingMachine('Beko', '123ABC')
 
-beko_washing_machine.turn_on()
-print(beko_washing_machine.producer, beko_washing_machine.model, beko_washing_machine.is_on)
-beko_washing_machine.turn_on()
-print(beko_washing_machine.producer, beko_washing_machine.model, beko_washing_machine.is_on)
+    whirlpool_washing_machine.turn_on()
+    print(whirlpool_washing_machine.producer, whirlpool_washing_machine.model, whirlpool_washing_machine.is_on)
+
+    print(updated_whirlpool_washing_machine.producer, updated_whirlpool_washing_machine.model,
+          updated_whirlpool_washing_machine.is_on)
+
+    beko_washing_machine.turn_on()
+    print(beko_washing_machine.producer, beko_washing_machine.model, beko_washing_machine.is_on)
+    beko_washing_machine.turn_on()
+    print(beko_washing_machine.producer, beko_washing_machine.model, beko_washing_machine.is_on)
+
+    updated_whirlpool_washing_machine.enable_program('daily')
+    updated_whirlpool_washing_machine.turn_on()
+    updated_whirlpool_washing_machine.enable_program('daily')
+    updated_whirlpool_washing_machine.display_current_status()
